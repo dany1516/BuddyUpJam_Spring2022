0d6ca6c452bd89db4e97e5edd083a85b0e0bb57d
dany1516 <filep.danielalexandru@gmail.com>
2022-05-05 16:12:06 +0200

    Shuffle the pieces in a rectangle feature added

--- Assets/Database/Scripts/JigsawPuzzleSO.cs
+++ Assets/Database/Scripts/JigsawPuzzleSO.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Jigsaw Puzzle", menuName = "Jigsaw/Create New Jigsaw Puzzle", order = 0)]
 public class JigsawPuzzleSO : ScriptableObject 
 {
-    [SerializeField] PuzzlePiece[] jigsawPiece;
     [SerializeField] int puzzleRows;
     [SerializeField] int puzzleColumns;
+    [SerializeField] float jigsawPieceSize;
+    [SerializeField] List<Sprite> jigsawPieces;
 
-    public PuzzlePiece[] GetJigsawPieces() => jigsawPiece;
     public int GetPuzzleRows() => puzzleRows;
     public int GetPuzzleColumns() => puzzleColumns;
+    public float GetJigsawPiecesSize() => jigsawPieceSize;
+    public List<Sprite> GetJigsawPieces() => jigsawPieces;
 }
 
 [System.Serializable]
 public class PuzzlePiece
 {
     public Sprite pieceSprite;
-    public Vector3 piecePosition;
 }
--- Assets/Scripts/CreateJigsaw.cs
+++ Assets/Scripts/CreateJigsaw.cs
@@ -9,39 +9,64 @@ public enum Tags
 
 public class CreateJigsaw : MonoBehaviour
 {
+    [Header("Pieces Values")]
     [SerializeField] JigsawPuzzleSO jigsawPuzzle;
     [SerializeField] Tags pieceTag;
-    [SerializeField] float zValue;
 
-    GameObject jigsawNewPiece;
+    [Header("Coordonates to spawn pieces")]
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+    List<GameObject> jigsawNewPiece;
 
     void Start()
     {
-        for(int i = 0; i < jigsawPuzzle.GetJigsawPieces().Length; i++)
+        jigsawNewPiece = new List<GameObject>(0);
+        CreatePieces();
+        ChangeValues();
+    }
+
+    private void ClearJigsaw()
+    {
+        jigsawNewPiece.Clear();
+    }
+
+    private void ChangeValues()
+    {
+        var piecePosition = new Vector3(0, 0, 0);
+
+        for(int i = 0; i < jigsawPuzzle.GetPuzzleRows(); i++) 
         {
-            jigsawNewPiece = new GameObject("JigsawPiece" + (i));
-            AddComponents(i);
-            ModifyAttributes(i);
-        }
+            for(int j = 0; j < jigsawPuzzle.GetPuzzleColumns(); j++) 
+            {
+                var currentPiece = jigsawPuzzle.GetPuzzleRows() * i + j;
+
+                jigsawNewPiece[currentPiece].tag = pieceTag.ToString();
+                jigsawNewPiece[currentPiece].transform.parent = this.gameObject.transform;
+                jigsawNewPiece[currentPiece].transform.position = this.gameObject.transform.position + 
+                    new Vector3(Random.Range(minX,maxX), Random.Range(minY,maxY),0);
+                piecePosition.x += jigsawPuzzle.GetJigsawPiecesSize();   
+            }
+            piecePosition.x = 0;   
+            piecePosition.y -= jigsawPuzzle.GetJigsawPiecesSize();
+        }    
     }
 
-    private void ModifyAttributes(int i)
+    private void CreatePieces()
     {
-        jigsawNewPiece.tag = pieceTag.ToString();
-        jigsawNewPiece.transform.parent = this.gameObject.transform;
-        jigsawNewPiece.transform.position = this.gameObject.transform.position + jigsawPuzzle.GetJigsawPieces()[i].piecePosition + new Vector3(-13,6,0);      
-    }
+        for(int i = 0; i < jigsawPuzzle.GetJigsawPieces().Count; i++)
+        {
+            var newPiece = new GameObject("JigsawPiece" + (i));
 
-    void RandomPosition()
-    {
-         
-    }
+            jigsawNewPiece.Add(newPiece);
+            newPiece.AddComponent<SpriteRenderer>().sprite = jigsawPuzzle.GetJigsawPieces()[i];
+            newPiece.AddComponent<BoxCollider2D>();
+            newPiece.AddComponent<MovePiece>();
 
-    private void AddComponents(int i)
-    {
-        jigsawNewPiece.AddComponent<SpriteRenderer>().sprite = jigsawPuzzle.GetJigsawPieces()[i].pieceSprite;
-        jigsawNewPiece.AddComponent<BoxCollider2D>();
-        jigsawNewPiece.AddComponent<MovePiece>();   
+            Debug.Log(newPiece.name);
+        } 
     }
 
     void Update()
